0dc4cee7eda42ea32026d8dfbfce5a3cb618271a
Yuuki23KMY <sbserxv@gmail.com>
2025-06-24 22:08:06 +0700

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,7 @@
+3  |  import ReactDOM from "react-dom/client";
+4  |  import App from "./App";
+5  |  import "./index.css";
+   |          ^
+6  |  ReactDOM.createRoot(document.getElementById("root")).render(
+7  |    /* @__PURE__ */ jsxDEV(React.StrictMode, { children: /* @__PURE__ */ jsxDEV(App, {}, void 0, false, {
+)
---
+++ utils.ts
@@ -0,0 +1,19 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatThaiDate(date: Date | string): string {
+  const dateObj = typeof date === 'string' ? new Date(date) : date;
+  return dateObj.toLocaleString('th-TH', {
+    timeZone: 'Asia/Bangkok',
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+  });
+}
